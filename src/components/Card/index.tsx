d50943222a120f87b61a4ce3841116ed78a5935a
Gedson Marcelino <gedson.marcelino@gmail.com>
2023-05-06 17:03:38 -0400

    refactor: card

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
-import { Container, Image, ImageButton, Price, PriceButton, PriceIcon, PriceText, Title } from './styles';
 import ImageDB from '../Image'
 import IconDB from '../Icon'
 
+import { Image, Button, Text, Row, View } from 'native-styled-wind';
+
 export const Card = ({ navigation, data }) => {
-  return (<Container>
-    <ImageButton onPress={() => navigation.navigate('View', { ...data })} >
-      <Image source={ImageDB[data.image].uri} />
-    </ImageButton>
-    <Title>{data.title}</Title>
-    <Price>
-      <PriceText>$ {data.price}</PriceText>
-      <PriceButton>
-        <PriceIcon source={IconDB['icon-add'].uri} />
-      </PriceButton>
-    </Price>
-  </Container>);
+  return (<View styles="w-{144} p-3 rounded-2xl justify-center" style={{ backgroundColor: '#362C36' }}>
+    <Button styles="w-{111} h-{111}  mb-2 " onPress={() => navigation.navigate('View', { ...data })} >
+      <Image styles="w-full h-{111} rounded-2xl" source={ImageDB[data.image].uri} />
+    </Button>
+    <Text styles="text-sm mb-3.5">{data.title}</Text>
+    <Row styles="p-{11} rounded-lg relative" style={{ backgroundColor: '#463D46' }}>
+      <Text styles="w-full text-center pr-10">$ {data.price}</Text>
+      <Button styles="rounded-lg p-4 absolute" style={{ backgroundColor: '#EFE3C8', top: 0, right: 0 }}>
+        <Image source={IconDB['icon-add'].uri} />
+      </Button>
+    </Row>
+  </View>);
 }
